1234d905f18b62bebb0cd2ea5dd15cf38dc4bfba
John Tafoya <johnt@competitiontechnology.com>
2021-05-15 16:29:59 -0700

    Figuring out how to do code-driven UI

--- PriceCalculator/ContentView.swift
+++ PriceCalculator/ContentView.swift
@@ -13,35 +13,26 @@ struct ContentView: View {
     @State private var width: String = ""
     @State private var p: String = ""
     @State private var price: Double = 0
+    @State private var selectedLiner = LinerBrand.Latham
 
     //let upchargeAmtExpected: Double = 177.00
     //let calc = LinerPriceCalculator(pool: nil, area: 0, linerBrand: LinerBrand.undefined)
-
-    func getPrice() {
-        let nf = NumberFormatter()
-        
-        let l = nf.number(from: length)
-        let w = nf.number(from: width)
-        
-        //price = (l!.doubleValue * w!.doubleValue)
-        
-        let pool = Rectangle(length: l!.doubleValue, width: w!.doubleValue)
-        print("areaPool = \(pool.areaPool)")
-        
-        let calc = LinerPriceCalculator(pool: pool, area: pool.areaPool, linerBrand: LinerBrand.Latham)
-        calc.calculatePrice()
-        
-        price = calc.priceResult.calculatedPrice
-        p = "$\(price)"
-    }
 
     var body: some View {
 //        let chgAmt = calc.getFreeformIrregularShapeChargeAmount()
 //        let s = DoubleHelper.roundToHundredth(value: chgAmt)
 //        Text("chgAmt=\(s)").padding()
         
-        Text("Get liner price for rectanglular pool and brand == 'Latham'").padding()
+        Text("Get liner price for rectanglular pool by brand").padding()
         Spacer()
+        
+        Picker("Liner", selection: $selectedLiner) {
+            Text("Latham").tag(LinerBrand.Latham)
+            Text("LathamModel2").tag(LinerBrand.LathamModel2)
+            Text("LathamModel3").tag(LinerBrand.LathamModel3)
+            Text("Premier").tag(LinerBrand.Premier)
+            Text("Signature").tag(LinerBrand.Signature)
+        }
         
         TextField("Length", text: $length).padding()
         TextField("Width", text: $width).padding()
@@ -54,6 +45,7 @@ struct ContentView: View {
 //                let w = nf.number(from: width)
 //                price = (l!.doubleValue * w!.doubleValue)
 //                p = "$\(price)"
+                print("selectedLinerStr='\($selectedLiner)'")
                 getPrice()
             }) {
                 Text("Get Price")
@@ -68,6 +60,28 @@ struct ContentView: View {
 //            Text("Toggler")
 //                .padding()
 //        }
+    }
+
+    //-----------------
+    //-----------------
+    func getPrice() {
+        let nf = NumberFormatter()
+        
+        let l = nf.number(from: length)
+        let w = nf.number(from: width)
+        
+        //price = (l!.doubleValue * w!.doubleValue)
+        
+        let pool = Rectangle(length: l!.doubleValue, width: w!.doubleValue)
+        print("areaPool = \(pool.areaPool)")
+        
+        let b: LinerBrand = selectedLiner
+        let calc = LinerPriceCalculator(pool: pool, area: pool.areaPool, linerBrand: b)
+        
+        calc.calculatePrice()
+        
+        price = calc.priceResult.calculatedPrice
+        p = "$\(price)"
     }
 }
 
